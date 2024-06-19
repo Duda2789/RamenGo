107f173b1db2d8b45791f28637b83e889c56b030
Duda2789 <137419172+Duda2789@users.noreply.github.com>
2024-06-18 22:17:03 -0300

    Update Program.cs

--- RamenGoAPI/Program.cs
+++ RamenGoAPI/Program.cs
@@ -1,39 +1,49 @@
 using System.Net;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
 
-var builder = WebApplication.CreateBuilder(args);
-
-
-builder.WebHost.ConfigureKestrel(serverOptions =>
+namespace RamenGoAPI
 {
-    serverOptions.Listen(IPAddress.Any, 5000);
-    serverOptions.Listen(IPAddress.Any, 5001, listenOptions =>
+    public class Program
     {
-        listenOptions.UseHttps();
-    });
-});
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllersWithViews();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+            builder.WebHost.ConfigureKestrel(serverOptions =>
+            {
+                serverOptions.Listen(IPAddress.Any, 5000);
+                serverOptions.Listen(IPAddress.Any, 5001, listenOptions =>
+                {
+                    listenOptions.UseHttps();
+                });
+            });
 
-var app = builder.Build();
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
 
+            var app = builder.Build();
 
-if (!app.Environment.IsDevelopment())
-{
-    app.UseHttpsRedirection();
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseHttpsRedirection();
+            }
+
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllers();
+            });
+
+            app.Run();
+        }
+    }
 }
-
-app.UseStaticFiles();
-app.UseRouting();
-app.UseAuthorization();
-
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=Home}/{action=Index}/{id?}");
-    endpoints.MapControllers();
-});
-
-app.Run();
